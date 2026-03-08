d219ee6b3d4442443df5a98dfe9e68a2b1b609d2
Hugo Luigi <104260964+hugoluigiss@users.noreply.github.com>
2026-03-08 14:28:40 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 5173,
-    allowedHosts: 'all',
+    allowedHosts: true,
   },
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
-    allowedHosts: 'all',
+    allowedHosts: true,
   },
 })
