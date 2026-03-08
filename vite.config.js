6c6026c45fccbb36aa17c87a5144850c692e019f
Hugo Luigi <104260964+hugoluigiss@users.noreply.github.com>
2026-03-08 14:24:43 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: 'all',
   },
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
+    allowedHosts: 'all',
   },
 })
